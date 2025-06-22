99b774d50da5d6dd447a2ebc3c965a8779764dd2
Maiky William <75399288+MaikyWilliam@users.noreply.github.com>
2025-06-21 21:36:20 -0300

    fix: ajuste da foto

--- assets/js/historico.js
+++ assets/js/historico.js
@@ -14,7 +14,7 @@ function carregarPaginaHistorico(dados) {
     dados.jogos.forEach(jogo => {
         const card = document.createElement('div');
         card.className = 'card card-ultimo-jogo';
-        const imagemDestaque = jogo.imagem_destaque ? `${jogo.imagem_destaque}` : 'https://source.unsplash.com/400x300/?soccer';
+        const imagemDestaque = jogo.imagem_destaque ? `assets/imagens/${jogo.imagem_destaque}` : 'https://source.unsplash.com/400x300/?soccer';
         const descricaoHtml = jogo.descricao || '';
         const descricaoTexto = descricaoHtml.replace(/<[^>]*>/g, '').substring(0, 120);
         card.innerHTML = `<a href="noticia.html?jogoId=${jogo.id}"><div class="card-imagem-container"><img src="${imagemDestaque}" alt="Imagem do jogo ${jogo.titulo}"><span class="card-data">${jogo.data || ''}</span></div><div class="card-conteudo-noticia"><h3>${jogo.titulo}</h3><p>${descricaoTexto}...</p><span class="card-saiba-mais">Saiba Mais...</span></div></a>`;
